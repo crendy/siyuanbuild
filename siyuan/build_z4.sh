88a13b641aa1f64538e6bb72d5cf12cccf1b03ef
yulixing <yulixing@meituan.com>
2024-07-11 17:19:45 +0800

    fix

--- siyuan/build_z4.sh
+++ siyuan/build_z4.sh
@@ -1,7 +1,7 @@
 ver=`head -n 1 CHANGELOG.md|awk -F " " '{print $2}'`
 echo "build siyuan $ver"
-docker build -f Dockerfile.z4 -t zsource/siyuan:$ver . 
+docker build -f Dockerfile.z4 -t crendy/siyuan:latest  -t crendy/siyuan:$ver .
 
-# docker push zsource/siyuan:$ver  
-# docker tag zsource/siyuan:$ver zsource/siyuan:latest
-# docker push zsource/siyuan:latest
+docker push docker.io/crendy/siyuan:$ver
+docker push docker.io/crendy/siyuan:latest
+# docker tag crendy/siyuan:$ver crendy/siyuan:latest
